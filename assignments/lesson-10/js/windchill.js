88c7af9d13c10f2f59608cfa243400b4f6cbefda
dimarussia <kob17002@byui.edu>
2019-03-16 17:21:25 +0300

    wind chill update

--- assignments/lesson-10/js/windchill.js
+++ assignments/lesson-10/js/windchill.js
@@ -1,5 +1,15 @@
-
+/*
 let t = parseInt(document.getElementById('value2').innerHTML);
 let s = parseInt(document.getElementById('value5').innerHTML);
 let result = 35.74+0.6215*t-35.75*(Math.pow(s,0.16))+0.4275*t*(Math.pow(s,0.16));
-document.getElementById('output').innerHTML = result.toFixed(1) + "F";
+document.getElementById('windchill').innerHTML = result.toFixed(1) + "F";
+*/
+
+let s = parseInt(document.getElementById("value5").innerHTML);
+let t = parseInt(document.getElementById("value2").innerHTML);
+
+
+// Processing - some random formula processing with the variable
+let result = 35.74 + .6215*t - 35.75* Math.pow(s,.16)+.4275 * t * Math.pow(s,.16);
+
+document.getElementById("windChill").innerHTML = "<strong>" + result.toFixed(1) + "</strong>";
